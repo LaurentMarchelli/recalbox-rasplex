4a817de95b7d70bd9a73ebbc517417e6a189c8c6
Laurent Marchelli <laurent.marchelli@gmail.com>
2016-05-03 00:41:24 +0200

    Partition tar.xz creation implemented

--- package/rasplex/rasplex.mk
+++ package/rasplex/rasplex.mk
@@ -15,32 +15,45 @@ RASPLEX_SITE = https://github.com/RasPlex/RasPlex/releases/download/$(RASPLEX_RE
 RASPLEX_LICENSE = GPL2
 RASPLEX_LICENSE_FILES = COPYING
 
-#define RASPLEX_EXTRACT_CMDS
-#	unzip -q -o $(DL_DIR)/$(RASPLEX_SOURCE) -d $(@D)/rasplex
-#endef
-
 RASPLEX_TRG_BUILD=$(@D)/os/$(RASPLEX_NAME)
-RASPLEX_TAR_BUILD=$(@D)/os.tar
+RASPLEX_TAR_BUILD=$(RASPLEX_TRG_BUILD).dsk
 
-define RASPLEX_BUILD_CMDS
-	if test -d $(RASPLEX_TAR_BUILD); then \
-		rm -rf $(RASPLEX_TAR_BUILD); \
-	fi; \
-	mkdir -p $(RASPLEX_TAR_BUILD)/plexboot $(RASPLEX_TAR_BUILD)/plexdata; \
-	cp -r $(@D)/3rdparty/bootloader/* $(RASPLEX_TAR_BUILD)/plexboot; \
-	cp $(@D)/target/SYSTEM $(RASPLEX_TAR_BUILD)/plexboot/SYSTEM; \
-	cp $(@D)/target/KERNEL $(RASPLEX_TAR_BUILD)/plexboot/kernel.img; \
-	if test -d $(RASPLEX_TRG_BUILD); then \
+# Prepare build directories :
+# $(@D)/os/rasplex      (Noobs configuration)
+# $(@D)/os/rasplex.dsk  (Partitions extracted)
+define RASPLEX_PRE_BUILD_CMDS
+	@if test -d $(RASPLEX_TRG_BUILD); then \
 		rm -rf $(RASPLEX_TRG_BUILD); \
 	fi; \
 	mkdir -p $(RASPLEX_TRG_BUILD); \
 	cp -r $(RASPLEX_PKGDIR)_resources/rpi/* $(RASPLEX_TRG_BUILD)/; \
-	cp -r $(RASPLEX_PKGDIR)_resources/$(BR2_ARCH)/* $(RASPLEX_TRG_BUILD)/;
+	cp -r $(RASPLEX_PKGDIR)_resources/$(BR2_ARCH)/* $(RASPLEX_TRG_BUILD)/; \
+	if test -d $(RASPLEX_TAR_BUILD); then \
+		rm -rf $(RASPLEX_TAR_BUILD); \
+	fi; \
+	mkdir -p $(RASPLEX_TAR_BUILD)/plexboot; \
+	cp -r $(@D)/3rdparty/bootloader/* $(RASPLEX_TAR_BUILD)/plexboot; \
+	cp $(@D)/target/SYSTEM $(RASPLEX_TAR_BUILD)/plexboot/SYSTEM; \
+	cp $(@D)/target/KERNEL $(RASPLEX_TAR_BUILD)/plexboot/kernel.img; \
+	mkdir -p $(RASPLEX_TAR_BUILD)/plexdata/lost+found;
 endef
 
+# Add configuration specifics to rasplex partitions
+define RASPLEX_BUILD_CMDS
+endef
+
+# Compress partitions into tar.xz
+define RASPLEX_POST_BUILD_CMDS
+	@for p in `ls -d $(RASPLEX_TAR_BUILD)/*/`; do \
+		pushd $$p > /dev/null; \
+		tar cfvJ $(RASPLEX_TRG_BUILD)/$$(basename $$p).tar.xz *; \
+		popd > /dev/null; \
+	done
+endef
+
+# Copy the content of the $(RASPLEX_TRG_BUILD) to the target/os/rasplex
 RASPLEX_SRC_INSTALL=$(RASPLEX_TRG_BUILD)
 RASPLEX_TRG_INSTALL=$(TARGET_DIR)/os/$(RASPLEX_NAME)
-
 define RASPLEX_INSTALL_TARGET_CMDS
 	#$(call $(RASPLEX_INSTALL_NOOBS))
 	@if test -d $(RASPLEX_TRG_INSTALL); then \
@@ -48,5 +61,8 @@ define RASPLEX_INSTALL_TARGET_CMDS
 	fi; \
 	cp -r $(RASPLEX_SRC_INSTALL) $(RASPLEX_TRG_INSTALL)
 endef
+
+RASPLEX_PRE_BUILD_HOOKS += RASPLEX_PRE_BUILD_CMDS
+RASPLEX_POST_BUILD_HOOKS += RASPLEX_POST_BUILD_CMDS
 
 $(eval $(generic-package))
