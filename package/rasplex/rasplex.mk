4b3164d623fdd97cc253d28bdc9b262146698899
Laurent Marchelli <laurent.marchelli@gmail.com>
2016-10-20 12:31:14 +0200

    Code review

--- package/rasplex/rasplex.mk
+++ package/rasplex/rasplex.mk
@@ -3,7 +3,6 @@
 # rasplex
 #
 ################################################################################
-
 RASPLEX_RELEASE = 1.6.2
 ifeq ($(BR2_ARCH),"rpi1")
 	RASPLEX_VERSION = $(RASPLEX_RELEASE).123-e23a7eef-RPi.arm
@@ -16,23 +15,28 @@ RASPLEX_LICENSE = GPL2
 RASPLEX_LICENSE_FILES = COPYING
 
 #####################################################################
-# 			PRE_BUILD : Prepare partition directories
-# 	$(@D)/os/rasplex      (Noobs configuration)
-# 	$(@D)/os/rasplex.dsk  (Partitions extracted)
+# 	CONFIGURE : Create noobs os directory for selected rpi
+# 	$(@D)/os/rasplex (Noobs configuration)
 #####################################################################
 RASPLEX_TRG_BUILD=$(@D)/os/$(RASPLEX_NAME)
-RASPLEX_TAR_BUILD=$(RASPLEX_TRG_BUILD).dsk
 
-define RASPLEX_PRE_BUILD_CMDS
-	if test -d $(RASPLEX_TRG_BUILD); then \
-		rm -rf $(RASPLEX_TRG_BUILD); \
-	fi; \
+define RASPLEX_CONFIGURE_CMDS
+	rm -rf $(RASPLEX_TRG_BUILD); \
 	mkdir -p $(RASPLEX_TRG_BUILD); \
 	cp -r $(RASPLEX_PKGDIR)noobs/rpi/* $(RASPLEX_TRG_BUILD)/; \
-	cp -r $(RASPLEX_PKGDIR)noobs/$(BR2_ARCH)/* $(RASPLEX_TRG_BUILD)/; \
-	if test -d $(RASPLEX_TAR_BUILD); then \
-		rm -rf $(RASPLEX_TAR_BUILD); \
-	fi; \
+	cp -r $(RASPLEX_PKGDIR)noobs/$(BR2_ARCH)/* $(RASPLEX_TRG_BUILD)/;
+endef
+
+#####################################################################
+# 	PRE_BUILD : Prepare partition directories
+# 	$(@D)/os/rasplex.dsk/* (Partitions extracted)
+#####################################################################
+RASPLEX_TAR_BUILD = $(RASPLEX_TRG_BUILD).dsk
+
+RASPLEX_PARTITIONS = plexboot plexdata
+
+define RASPLEX_PRE_BUILD_CMDS
+	rm -rf $(RASPLEX_TAR_BUILD); \
 	mkdir -p $(RASPLEX_TAR_BUILD)/plexboot; \
 	cp -r $(@D)/3rdparty/bootloader/* $(RASPLEX_TAR_BUILD)/plexboot; \
 	cp $(@D)/target/* $(RASPLEX_TAR_BUILD)/plexboot/; \
@@ -42,51 +46,50 @@ define RASPLEX_PRE_BUILD_CMDS
 endef
 
 #####################################################################
-# 			BUILD : Customize partition directories
+# 	BUILD : Customize partition directories
 #####################################################################
-# Powerswitch configuration
 RASPLEX_POWER_PATH = $(RASPLEX_TAR_BUILD)/plexdata/.cache/services
 define RASPLEX_BUILD_POWER_CMD
-	if [ '$(BR2_ARCH_POWERSWITCH_REMOTEPI_2013)' ==  'y' ]; then \
+	# Powerswitch configuration
+	$(if $(BR2_ARCH_POWERSWITCH_REMOTEPI_2013), \
 		mkdir -p $(RASPLEX_POWER_PATH); \
 		echo BOARD_VERSION=\"2013\" > $(RASPLEX_POWER_PATH)/remotepi-board.conf; \
-	elif [ '$(BR2_ARCH_POWERSWITCH_REMOTEPI_2015)' ==  'y' ]; then \
+	)
+	$(if $(BR2_ARCH_POWERSWITCH_REMOTEPI_2015), \
 		mkdir -p $(RASPLEX_POWER_PATH); \
 		echo BOARD_VERSION=\"2015\" > $(RASPLEX_POWER_PATH)/remotepi-board.conf; \
-	fi;
+	)
 endef
 
 # Skin configuration
-ifndef BR2_PACKAGE_RASPLEX_SKIN_OPENPHT
-	ifdef BR2_PACKAGE_RASPLEX_SKIN_AEONNOX
-		RASPLEX_SKIN_NAME = skin.aeon.nox.5
-		RASPLEX_SKIN_VERSION = 5.2.3
-	endif
+ifdef BR2_PACKAGE_RASPLEX_SKIN_AEONNOX
+	RASPLEX_SKIN_NAME = skin.aeon.nox.5
+	RASPLEX_SKIN_VERSION = 5.2.3
 	# URL information stored in /storage/.plexht/userdata/Database/Addons15.db
 	RASPLEX_SKIN_SITE = https://addons.openpht.tv/openpht-1.6
 	RASPLEX_SKIN_FILE = $(RASPLEX_SKIN_NAME)-$(RASPLEX_SKIN_VERSION).zip
-	RASPLEX_SKIN_PATH = $(RASPLEX_TAR_BUILD)/plexdata/.plexht/addons
-	RASPLEX_USER_PATH = $(RASPLEX_TAR_BUILD)/plexdata/.plexht/userdata
 	# Download skin if required by configuration
 	RASPLEX_EXTRA_DOWNLOADS = \
 		$(RASPLEX_SKIN_SITE)/$(RASPLEX_SKIN_NAME)/$(RASPLEX_SKIN_FILE)
 endif
 
 define RASPLEX_BUILD_SKIN_CMD
-	if [ '$(BR2_PACKAGE_RASPLEX_SKIN_OPENPHT)' !=  'y' ]; then \
-		mkdir -p $(RASPLEX_SKIN_PATH)/; \
-		unzip -q -o $(DL_DIR)/$(RASPLEX_SKIN_FILE) -d $(RASPLEX_SKIN_PATH)/; \
-		if [ '$(BR2_PACKAGE_RECALBOXOS)' ==  'y' ]; then \
-			mkdir -p $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/; \
-			cp -r $(RASPLEX_PKGDIR)recalplex/* $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/; \
-			cp -r $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/addons/$(RASPLEX_SKIN_NAME)/* \
-				$(RASPLEX_SKIN_PATH)/$(RASPLEX_SKIN_NAME)/; \
-			mkdir -p $(RASPLEX_USER_PATH); \
-			cp -r $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/userdata/$(RASPLEX_SKIN_NAME)/* \
-				$(RASPLEX_USER_PATH)/; \
-			cat $(RASPLEX_PKGDIR)noobs/partition_setup.txt >> $(RASPLEX_TRG_BUILD)/partition_setup.sh; \
-		fi; \
-	fi;
+	$(if $(and $(BR2_PACKAGE_RECALBOXOS), $(BR2_PACKAGE_RASPLEX_SKIN_AEONNOX)), \
+		# Unzip and customize rasplex skin 
+		mkdir -p $(RASPLEX_TAR_BUILD)/plexdata/.plexht/addons/; \
+		unzip -q -o $(DL_DIR)/$(RASPLEX_SKIN_FILE) -d $(RASPLEX_TAR_BUILD)/plexdata/.plexht/addons/; \
+		cp -r $(RASPLEX_PKGDIR)recalplex/addons/$(RASPLEX_SKIN_NAME)/* \
+			$(RASPLEX_TAR_BUILD)/plexdata/.plexht/addons/$(RASPLEX_SKIN_NAME)/;
+		# Copy customized rasplex skin settings 
+		mkdir -p $(RASPLEX_TAR_BUILD)/plexdata/.plexht/userdata/; \
+		cp -r $(RASPLEX_PKGDIR)recalplex/userdata/$(RASPLEX_SKIN_NAME)/* \
+			$(RASPLEX_TAR_BUILD)/plexdata/.plexht/userdata/;
+		# Copy recalplex directory with autoswitch scripts and slideshow images
+		mkdir -p $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/; \
+		cp -r $(RASPLEX_PKGDIR)recalplex/* $(RASPLEX_TAR_BUILD)/plexdata/.recalplex/;
+		# Customize noobs partition setup
+		cat $(RASPLEX_PKGDIR)noobs/partition_setup.txt >> $(RASPLEX_TRG_BUILD)/partition_setup.sh; \
+	)
 endef
 
 define RASPLEX_BUILD_CMDS
@@ -98,11 +101,12 @@ endef
 # 	POST_BUILD : Compress partitions directories into tar.xz
 #####################################################################
 define RASPLEX_POST_BUILD_CMDS
-	for p in `ls -d $(RASPLEX_TAR_BUILD)/*/`; do \
-		pushd $$p > /dev/null; \
-		tar cfvJ $(RASPLEX_TRG_BUILD)/$$(basename $$p).tar.xz . --owner=0 --group=0; \
+	$(foreach p, $(RASPLEX_PARTITIONS), \
+		pushd $(RASPLEX_TAR_BUILD)/$(p) > /dev/null; \
+		f=$(RASPLEX_TRG_BUILD)/$(p).tar; \
+		tar uf $$f . --owner=0 --group=0; xz -v $$f; \
 		popd > /dev/null; \
-	done
+	)
 endef
 
 RASPLEX_PRE_BUILD_HOOKS += RASPLEX_PRE_BUILD_CMDS
@@ -115,10 +119,7 @@ RASPLEX_POST_BUILD_HOOKS += RASPLEX_POST_BUILD_CMDS
 RASPLEX_SRC_INSTALL=$(RASPLEX_TRG_BUILD)
 RASPLEX_TRG_INSTALL=$(TARGET_DIR)/os/$(RASPLEX_NAME)
 define RASPLEX_INSTALL_TARGET_CMDS
-	#$(call $(RASPLEX_INSTALL_NOOBS))
-	if test -d $(RASPLEX_TRG_INSTALL); then \
-		rm -rf $(RASPLEX_TRG_INSTALL); \
-	fi; \
+	rm -rf $(RASPLEX_TRG_INSTALL); \
 	cp -r $(RASPLEX_SRC_INSTALL) $(RASPLEX_TRG_INSTALL);
 endef
 
--- package/recalboxOS/recalboxOS.mk
+++ package/recalboxOS/recalboxOS.mk
@@ -3,7 +3,6 @@
 # recalboxos
 #
 ################################################################################
-
 RECALBOXOS_RELEASE = 4.0.0-beta5
 RECALBOXOS_SOURCE = $(RECALBOXOS_NAME)-$(RECALBOXOS_RELEASE).zip
 RECALBOXOS_SITE = https://github.com/recalbox/recalbox-os/releases/download/$(RECALBOXOS_RELEASE)
@@ -28,66 +27,78 @@ endif
 RECALBOXOS_SRC_BUILD=$(@D)/os/$(RECALBOXOS_NAME)-$(BR2_ARCH)
 
 define RECALBOXOS_EXTRACT_CMDS
-	unzip -q -o $(DL_DIR)/$(RECALBOXOS_SOURCE) -d $(@D); \
-	if test -e $(DL_DIR)/boot.tar.xz; then \
+	unzip -q -o $(DL_DIR)/$(RECALBOXOS_SOURCE) -d $(@D);
+	# Apply update patches
+	$(if $(RECALBOXOS_EXTRA_DOWNLOADS),\
 		mv $(DL_DIR)/boot.tar.xz $(RECALBOXOS_SRC_BUILD)/; \
-	fi; \
-	if test -e $(DL_DIR)/root.tar.xz; then \
 		mv $(DL_DIR)/root.tar.xz $(RECALBOXOS_SRC_BUILD)/; \
-	fi;
+	)
 endef
 
 #####################################################################
-# 			PRE_BUILD : Prepare partition directories
-# 	$(@D)/os/rasplex      (Noobs configuration)
-# 	$(@D)/os/rasplex.dsk  (Partitions extracted)
+# 	CONFIGURE : Create noobs os directory for selected rpi
+# 	$(@D)/os/rasplex (Noobs configuration)
 #####################################################################
 RECALBOXOS_TRG_BUILD=$(@D)/os/$(RECALBOXOS_NAME)
-RECALBOXOS_TAR_BUILD=$(RECALBOXOS_TRG_BUILD).dsk
 
-RECALBOXOS_FILE_recalbox_conf=recalbox/share_init/system/recalbox.conf
-RECALBOXOS_PART_root+= $(RECALBOXOS_FILE_recalbox_conf)
-
-define RECALBOXOS_PRE_BUILD_CMDS
-	if test -d $(RECALBOXOS_TRG_BUILD); then \
-		rm -rf $(RECALBOXOS_TRG_BUILD); \
-	fi; \
+define RECALBOXOS_CONFIGURE_CMDS
+	rm -rf $(RECALBOXOS_TRG_BUILD); \
 	mkdir -p $(RECALBOXOS_TRG_BUILD); \
 	cp -r $(RECALBOXOS_SRC_BUILD)/* $(RECALBOXOS_TRG_BUILD)/; \
 	mv $(RECALBOXOS_TRG_BUILD)/$(RECALBOXOS_NAME)-$(BR2_ARCH).png \
 		$(RECALBOXOS_TRG_BUILD)/$(RECALBOXOS_NAME).png; \
 	$(SED) "s|^\(\s*\)\"name\": \"$(RECALBOXOS_NAME)-$(BR2_ARCH)\",.*|\1\"name\": \"$(RECALBOXOS_NAME)\",|g" \
-		$(RECALBOXOS_TRG_BUILD)/os.json; \
-	if test -d $(RECALBOXOS_TAR_BUILD); then \
-		rm -rf $(RECALBOXOS_TAR_BUILD); \
-	fi; \
-	mkdir -p $(RECALBOXOS_TAR_BUILD)/root; \
-	pushd $(RECALBOXOS_TAR_BUILD)/root > /dev/null; \
-	unxz -v $(RECALBOXOS_TRG_BUILD)/root.tar.xz; \
-	tar xf $(RECALBOXOS_TRG_BUILD)/root.tar ./recalbox/share_init/system/recalbox.conf; \
-	popd> /dev/null; 
+		$(RECALBOXOS_TRG_BUILD)/os.json;
 endef
 
 #####################################################################
-# 			BUILD : Customize partition directories
+# 	PRE_BUILD : Prepare partition directories
+# 	$(@D)/os/rasplex.dsk/* (Partitions extracted)
 #####################################################################
-# Powerswitch configuration
+RECALBOXOS_TAR_BUILD=$(RECALBOXOS_TRG_BUILD).dsk
+
+RECALBOXOS_FILE_recalbox_conf=recalbox/share_init/system/recalbox.conf
+
+RECALBOXOS_PART_boot=
+RECALBOXOS_PART_root=$(RECALBOXOS_FILE_recalbox_conf)
+RECALBOXOS_PART_share=
+RECALBOXOS_PARTITIONS=root boot share
+
+define RECALBOXOS_PRE_BUILD_CMDS
+	rm -rf $(RECALBOXOS_TAR_BUILD); \
+	$(foreach p, $(RECALBOXOS_PARTITIONS), \
+		$(if $(RECALBOXOS_PART_$(p)), \
+			mkdir -p $(RECALBOXOS_TAR_BUILD)/$(p); \
+			pushd $(RECALBOXOS_TAR_BUILD)/$(p) > /dev/null; \
+			unxz -v $(RECALBOXOS_TRG_BUILD)/$(p).tar.xz; \
+			for f in $(RECALBOXOS_PART_$(p)); do \
+				tar xvf $(RECALBOXOS_TRG_BUILD)/$(p).tar ./$$f; \
+			done; \
+			popd > /dev/null; \
+		) \
+	)
+endef
+
+#####################################################################
+# 	BUILD : Customize partition directories
+#####################################################################
 define RECALBOXOS_BUILD_POWER_CMD
-	if [ '$(BR2_ARCH_POWERSWITCH_REMOTEPI_2013)' ==  'y' ]; then \
+	# Powerswitch configuration
+	$(if $(BR2_ARCH_POWERSWITCH_REMOTEPI_2013), \
 		$(SED) "s|^.*\(system.power.switch\)\s*=\s*\(REMOTEPIBOARD_20[0-1]3\s*.*\)$$|\1=\2|g" \
-			$(RECALBOXOS_TAR_BUILD)/root/$(RECALBOXOS_FILE_recalbox_conf); \
-	elif [ '$(BR2_ARCH_POWERSWITCH_REMOTEPI_2015)' ==  'y' ]; then \
+			$(RECALBOXOS_TAR_BUILD)/root/$(RECALBOXOS_FILE_recalbox_conf);\
+	)
+	$(if $(BR2_ARCH_POWERSWITCH_REMOTEPI_2015), \
 		$(SED) "s|^.*\(system.power.switch\)\s*=\s*\(REMOTEPIBOARD_20[0-1]5\s*.*\)$$|\1=\2|g" \
-			$(RECALBOXOS_TAR_BUILD)/root/$(RECALBOXOS_FILE_recalbox_conf); \
-	fi;
+			$(RECALBOXOS_TAR_BUILD)/root/$(RECALBOXOS_FILE_recalbox_conf);\
+	)
 endef
 
 define RECALBOXOS_BUILD_SKIN_CMD
-	if [ '$(BR2_RASPLEX_RECALBOXOS_SKIN_OPENPHT)' !=  'y' ]; then \
-		if [ '$(BR2_PACKAGE_RASPLEX)' ==  'y' ]; then \
-			cat $(RECALBOXOS_PKGDIR)noobs/partition_setup.txt >> $(RECALBOXOS_TRG_BUILD)/partition_setup.sh; \
-		fi; \
-	fi;
+	# Customize noobs partition setup
+	$(if $(and $(BR2_PACKAGE_RASPLEX), $(BR2_PACKAGE_RASPLEX_SKIN_AEONNOX)), \
+		cat $(RECALBOXOS_PKGDIR)noobs/partition_setup.txt >> $(RECALBOXOS_TRG_BUILD)/partition_setup.sh; \
+	)
 endef
 
 define RECALBOXOS_BUILD_CMDS
@@ -99,12 +110,14 @@ endef
 # 	POST_BUILD : Update partitions tar files if needed
 #####################################################################
 define RECALBOXOS_POST_BUILD_CMDS
-	for p in `ls -d $(RECALBOXOS_TAR_BUILD)/*/`; do \
-		pushd $$p > /dev/null; \
-		f=$(RECALBOXOS_TRG_BUILD)/$$(basename $$p).tar; \
-		tar uf $$f . --owner=0 --group=0; xz -v $$f; \
-		popd > /dev/null; \
-	done
+	$(foreach p, $(RECALBOXOS_PARTITIONS), \
+		$(if $(RECALBOXOS_PART_$(p)), \
+			pushd $(RECALBOXOS_TAR_BUILD)/$(p) > /dev/null; \
+			f=$(RECALBOXOS_TRG_BUILD)/$(p).tar; \
+			tar uf $$f . --owner=0 --group=0; xz -v $$f; \
+			popd > /dev/null; \
+		) \
+	)
 endef
 
 RECALBOXOS_PRE_BUILD_HOOKS += RECALBOXOS_PRE_BUILD_CMDS
@@ -112,7 +125,7 @@ RECALBOXOS_POST_BUILD_HOOKS += RECALBOXOS_POST_BUILD_CMDS
 
 #####################################################################
 # 	INSTALL : Copy the content of the $(RECALBOXOS_TRG_BUILD)
-#	          to the target/os/rasplex
+#	          to the target/os/recalboxOS
 #####################################################################
 ifeq ($(BR2_PACKAGE_NOOBS),y)
 	RECALBOXOS_DEPENDENCIES=noobs
@@ -126,9 +139,7 @@ RECALBOXOS_SRC_INSTALL=$(RECALBOXOS_TRG_BUILD)
 RECALBOXOS_TRG_INSTALL=$(TARGET_DIR)/os/$(RECALBOXOS_NAME)
 define RECALBOXOS_INSTALL_TARGET_CMDS
 	$(call $(RECALBOXOS_INSTALL_NOOBS))
-	if test -d $(RECALBOXOS_TRG_INSTALL); then \
-		rm -rf $(RECALBOXOS_TRG_INSTALL); \
-	fi; \
+	rm -rf $(RECALBOXOS_TRG_INSTALL); \
 	cp -r $(RECALBOXOS_SRC_INSTALL) $(RECALBOXOS_TRG_INSTALL) 
 endef
 
