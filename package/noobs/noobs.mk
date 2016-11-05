3c5e5a820467d7e083cc0a865dd1ca4913eeaff9
Joseph Weigl <joseph.weigl@gmail.com>
2016-11-05 18:12:50 +0100

    Update to noobs 2.0.0

--- package/noobs/noobs.mk
+++ package/noobs/noobs.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-NOOBS_VERSION = v1_9_3
+NOOBS_VERSION = v2_0_0
 NOOBS_SOURCE = NOOBS_$(NOOBS_VERSION).zip
-NOOBS_SITE = https://downloads.raspberrypi.org/NOOBS/images/NOOBS-2016-09-28
+NOOBS_SITE = https://downloads.raspberrypi.org/NOOBS/images/NOOBS-2016-10-05
 #NOOBS_SITE = https://downloads.raspberrypi.org/NOOBS_latest
 NOOBS_LICENSE = GPL2
 NOOBS_LICENSE_FILES = COPYING
